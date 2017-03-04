94aa145185f11ac08b5927c1f721ec986beb115a
Haruna <haruna0510@kif.biglobe.ne.jp>
2017-03-04 11:57:24 +0900

    Moved 'fortune cookie'

---
+++ projects/meadowlark.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var app = express();
+
+app.set('port', process.env.PORT || 3000);
+
+app.get('/', function (req, res) {
+    res.render('home');
+});
+
+app.get('/about', function (req, res) {
+    var randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
+    res.render('about', {fortune: randomFortune});
+});
+
+app.use(function (req, res) {
+    res.type('text/plain');
+    res.status(400);
+    res.send('404 - Not Found');
+});
+
+app.use(function (err, req, res, next) {
+    console.error(err.stack);
+    res.type('text/plain');
+    res.status(500);
+    res.send('500 - Server Error');
+});
+
+app.listen(app.get('port'), function () {
+    console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
+});
+
+var app = express();
+
+var handlebars = require('express-handlebars').create({defaultLayout: 'main'});
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+var fortunes = [
+    "Conquer your fears or they will conquer you.", "Rivers need springs.", "Do not fear what you don't know.", "You will have a pleasant surprise.", "Whenever possible, keep it simple."
+];
+

254d04371587b496ccc37a8da8063a0ee1ff41b7
Haruna <haruna0510@kif.biglobe.ne.jp>
2017-03-04 12:58:29 +0900

    Moved 'fortune cookie'

--- projects/meadowlark.js
+++ projects/meadowlark.js
@@ -1,3 +1,4 @@
+var fortune = require('./lib/fortune.js');
 var express = require('express');
 var app = express();
 
@@ -8,8 +9,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/about', function (req, res) {
-    var randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
-    res.render('about', {fortune: randomFortune});
+    res.render('about', {fortune: fortune.getFortune()});
 });
 
 app.use(function (req, res) {
@@ -34,8 +34,4 @@ var app = express();
 var handlebars = require('express-handlebars').create({defaultLayout: 'main'});
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
-
-var fortunes = [
-    "Conquer your fears or they will conquer you.", "Rivers need springs.", "Do not fear what you don't know.", "You will have a pleasant surprise.", "Whenever possible, keep it simple."
-];
 
